f643a6096e436b522bc05362bdc9b4e342cdf27d
shinagupta <132837582+shinagupta@users.noreply.github.com>
2024-10-29 20:47:44 +0530

    Create BankingAppApplication.java

---
+++ src/main/java/com/example/bankingapp/BankingAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.bankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankingAppApplication.class, args);
+    }
+}
